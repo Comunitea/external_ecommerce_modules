d512f62cff3096822c54f03594a4ec234cf3ca67
neo-oien <i@oien.ru>
2019-03-26 10:25:20 +0100

    [UPD] Breadcrumbs: field name for website.page

--- breadcrumbs_base/models/page.py
+++ breadcrumbs_base/models/page.py
@@ -8,5 +8,5 @@ from odoo import api, models, fields, _
 class Page(models.Model):
     _inherit = 'website.page'
 
+    name = fields.Char(string=_('Page name'), required=True, translate=True)
     parent_id = fields.Many2one('website.page', string=_("Parent page"))
-
