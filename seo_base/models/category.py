1b3dd886a3e2586de59a7324fcaef357b06e25fd
neo-oien <i@oien.ru>
2019-04-29 15:07:54 +0200

    [FIX] SEO: category friendly url field validation

--- seo_base/models/category.py
+++ seo_base/models/category.py
@@ -16,18 +16,23 @@ class ECommerceCategory(models.Model):
         for record in self:
             has_slug = values.get('slug', False)
             if not has_slug or has_slug == '':
-                # If slug not exists or is empty -> create from category name & id and validate it
-                new_slug = '%s-%s' % (record.name, record.id)
+                # If slug not exists or is empty -> create from product name & id and validate
+                if not record.slug:
+                    new_slug = '%s-%s' % (record.name, record.id)
+                else:
+                    new_slug = record.slug
                 values.update({
-                    'slug': ProductMeta._slug_validation(record, new_slug)
+                    'slug': ProductMeta._slug_validation(self, new_slug)
                 })
             else:
                 # If slug exists -> validate
                 values.update({
-                    'slug': ProductMeta._slug_validation(record, has_slug)
+                    'slug': ProductMeta._slug_validation(self, has_slug)
                 })
             # Write
-            return super(ECommerceCategory, record).write(values)
+            super(ECommerceCategory, record).write(values)
+            values.pop('slug')
+        return True
 
     @api.model
     def create(self, values):
