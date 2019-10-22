a3dfaa20a933abaf22ba82d7a3c61edbd7f02a47
javierjcf <javierjcf@gmail.com>
2019-10-22 18:55:53 +0200

    [FIX]revi_reviews: Error bucle infinito 2

--- revi_reviews/models/account_invoice.py
+++ revi_reviews/models/account_invoice.py
@@ -207,10 +207,9 @@ class AccountInvoice(models.Model):
                     # Set new Revi state for changed account invoice
                     if success:
                         res.sudo().write({'revi_state': 'sent'})
-                        super(AccountInvoice, res).write({'revi_state': 'sent'})
+                        # super(AccountInvoice, res).write({'revi_state': 'sent'})
                     else:
                         res.sudo().write({'revi_state': 'error'})
-                        super(AccountInvoice, res).write({'revi_state': 'error'})
+                        # super(AccountInvoice, res).write({'revi_state': 'error'})
 
-                return super(AccountInvoice, res).write(values)
         return super(AccountInvoice, self).write(values)
