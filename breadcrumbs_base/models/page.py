7220bc5bb25b4132d137d4296ddd093b66c609eb
ruben <ruben@comunitea.com>
2020-08-31 13:35:09 +0200

    [UDP] Prevent web editor error on new pages for required page name on breadcrumbs

--- breadcrumbs_base/models/page.py
+++ breadcrumbs_base/models/page.py
@@ -6,5 +6,5 @@ from odoo import api, models, fields, _
 class Page(models.Model):
     _inherit = 'website.page'
 
-    name = fields.Char(string=_('Page name'), required=True, translate=True)
+    name = fields.Char(string=_('Page name'), required=True, translate=True, default='')
     parent_id = fields.Many2one('website.page', string=_("Parent page"))
