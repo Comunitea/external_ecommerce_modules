d3c4575fe2550ee712075f7eb667f94544cc3ceb
vicentecom <vicente@comunitea.com>
2021-06-15 15:59:56 +0200

    [IMP] website_blog_base: El campo introduction ahora es traducible

--- website_blog_base/models/blog_post.py
+++ website_blog_base/models/blog_post.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, models, fields, tools, _
+from odoo.tools.translate import translate
 
 
 class BlogPost(models.Model):
     _inherit = 'blog.post'
 
     image = fields.Binary(attachment=True, string=_("Post main image"), help=_("This is image of post preview"))
-    introduction = fields.Text(string=_("Short post description"))
+    introduction = fields.Text(string=_("Short post description"), translate=True)
